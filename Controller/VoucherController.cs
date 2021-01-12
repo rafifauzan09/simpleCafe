15e0bd2525dc2fd0ebc3afac1e7e913a44211b10
rafifauzan09 <rafif.almahdy@students.amikom.ac.id>
2021-01-12 10:38:54 +0700

    Add project files.

---
+++ Controller/VoucherController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using simpleCafe.Model;
+
+namespace simpleCafe.Controller
+{
+    class VoucherController
+    {
+        private List<Voucher> items;
+
+        public VoucherController()
+        {
+            items = new List<Voucher>();
+        }
+
+        public void addItem(Voucher item)
+        {
+            this.items.Add(item);
+        }
+
+        public List<Voucher> getItems()
+        {
+            return this.items;
+        }
+
+    }
+}
